a18801f0925e4b9f1cef204c5af484e873aac1f6
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-09-06 20:37:01 -0300

    Next topic: swap two variables

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -m "project inserted"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/Java-Personal-Studies.git
+git commit -m "Next topic: swap two variables"
 git push -u origin main
 git status
+
+# continue in: https://youtu.be/xk4_1vDrzzo?si=JbcM1YK9JSSQ0fpl&t=1983
---
+++ src/scripts/script01.java
@@ -0,0 +1,11 @@
+package scripts;
+
+public class script01 {
+
+    public static void main(String[] args) {
+        int num01 = 14;
+        double num02 = 16.5;
+
+        System.out.println("Sum result: " + (num01 + num02));
+    }
+}
