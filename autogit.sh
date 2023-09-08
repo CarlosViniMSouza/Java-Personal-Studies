3d83ede0f66d44fc885f0e9742b5a17611c8804e
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-09-08 00:24:35 -0300

    Next topic: Math Class

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -m "Next topic: GUI Intro"
+git commit -m "Next topic: Math Class"
 git push -u origin main
 git status
 
-# continue in: https://youtu.be/xk4_1vDrzzo?si=SmiDdGLdj4fhvr8q&t=3220
+# continue in: https://youtu.be/xk4_1vDrzzo?si=dIBU0A_-5Q-b2-8F&t=3310
